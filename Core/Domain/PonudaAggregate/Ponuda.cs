579e8f74ab2b0e13db99d490871f01678974a22b
nikolabuzej <nbuzej@gmail.com>
2022-02-26 09:38:23 +0100

    Add ponuda repository interface and modify ponuda aggregate.

--- Core/Domain/PonudaAggregate/Ponuda.cs
+++ Core/Domain/PonudaAggregate/Ponuda.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Domain.BankaAggregate;
 using Core.Domain.InformacijeOIsporuciAggregate;
 using Core.Domain.JavniPozivAggregate;
+using Core.Domain.PonudjacAggregate;
 using Core.Domain.ProizvodAggregate;
 using Core.Extensions;
 
@@ -9,24 +10,59 @@ namespace Core.Domain.PonudaAggregate
     public enum StatusPonude { URazmatranju, Prihvacena,  Odbijena };
     public class Ponuda
     {
+        public Ponuda(Kontakt kontakt,
+                     DateTime datumPristizanja,
+                     string zakonskiZastupnik,
+                     StatusPonude status,
+                     JavniPoziv javniPoziv,
+                     Ponudjac ponudjac,
+                     InformacijeOIsporuci informacijeOIsporuci)
+        {
+            Kontakt = kontakt;
+            DatumPristizanja = datumPristizanja;
+            ZakonskiZastupnik = zakonskiZastupnik;
+            Status = status;
+            JavniPoziv = javniPoziv;
+            Ponudjac = ponudjac;
+            InformacijeOIsporuci = informacijeOIsporuci;
+        }
+
         public Guid Id { get; init; } = Guid.NewGuid();
-        public Kontakt Kontakt { get; set; } = new();
-        public DateTime DatumPristizanja { get; set; } = DateTime.Now;
-        public string ZakonskiZastupnik { get; set; } = string.Empty;
-        public StatusPonude Status { get; set; }
-        public JavniPoziv JavniPoziv { get; set; } = new();
-        public InformacijeOIsporuci InformacijeOIsporuci { get; set; } = new();
+        public Kontakt Kontakt { get; private set; } = new();
+        public DateTime DatumPristizanja { get; private set; } = DateTime.Now;
+        public string ZakonskiZastupnik { get; private set; } = string.Empty;
+        public StatusPonude Status { get; private set; }
+        public JavniPoziv JavniPoziv { get; private set; } = new();
+        public Ponudjac Ponudjac { get; private set; } = new();
+        public InformacijeOIsporuci InformacijeOIsporuci { get; private set; } = new();
 
         private readonly List<StavkaStruktureCene> _stavkeStruktureCene = new();
         public IReadOnlyCollection<StavkaStruktureCene> StavkeStruktureCene => _stavkeStruktureCene;
 
         private readonly List<TekuciRacunPonudjaca> _tekuciRacuniPonudjaca = new();
         public IReadOnlyCollection<TekuciRacunPonudjaca> TekuciRacuniPonudjaca => _tekuciRacuniPonudjaca;
-
+     
+        public void Update(Kontakt kontakt,
+                     DateTime datumPristizanja,
+                     string zakonskiZastupnik,
+                     StatusPonude status,
+                     JavniPoziv javniPoziv,
+                     Ponudjac ponudjac,
+                     InformacijeOIsporuci informacijeOIsporuci)
+        {
+            Kontakt = kontakt;
+            DatumPristizanja = datumPristizanja;
+            ZakonskiZastupnik = zakonskiZastupnik;
+            Status = status;
+            JavniPoziv = javniPoziv;
+            Ponudjac = ponudjac;
+            InformacijeOIsporuci = informacijeOIsporuci;
+        }
         public void DodajStavkuStruktureCene(int kolicina, double jedinicnaCenaBezPdv, double jedinicnaCenaSaPdv, Proizvod proizvod)
         {
             _stavkeStruktureCene.Add(new()
             {
+                Kolicina = kolicina,
                 JedinicnaCenaBezPdv = jedinicnaCenaBezPdv,
                 JedinicnaCenaSaPdv = jedinicnaCenaSaPdv,
                 Proizvod = proizvod
@@ -47,7 +83,7 @@ namespace Core.Domain.PonudaAggregate
           _stavkeStruktureCene.Remove(stavka);
         }
 
-        public void ObrisiTekuciRacun(Guid id)
+        public void ObrisiTekuciRacunPonudjaca(Guid id)
         {
             var racun  = _tekuciRacuniPonudjaca.FirstOrDefault(x => x.Id == id).EnsureExists();
 
---
+++ Core/Domain/PonudaAggregate/Repositories/IPonudaRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.PonudaAggregate.Repositories
+{
+    public interface IPonudaRepository
+    {
+        public Task<Ponuda> VratiPonudu(Guid id);
+        public Task KreirajPonudu(Ponuda ponuda);
+        public Task AzurirajPonudu(Ponuda ponuda);
+        public Task IzbrisiPonudu(Guid id);
+    } 
+}
